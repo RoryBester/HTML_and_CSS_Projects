48b7823a93cde26d1ac23770b24b8ff3d3fc9205
RoryBester <88057657+RoryBester@users.noreply.github.com>
2021-08-12 15:09:02 -0400

    added
    photos and button attribute
    
    bold attribute
    
    italic attribute
    
    under line attribute

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Dogs are awesome!");
+	function My_First_Function() {
+	    var string = "kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
